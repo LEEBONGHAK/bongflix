5f0f51a747c6c09251be74bd1223bb237533d15b
octo <dlqhdgkr456@gmail.com>
2021-09-04 20:57:40 +0900

    3.2 CSS in React part Three

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-d
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
+import Header from "./Header";
 
 export default () => (
   <Router>
-    {/* Switch : 한번에 오직 하나의 Route만 Render하라 */}
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" component={Search} />
-      {/* 일치하는 Route가 없다면 Redirect 해라 */}
-      <Redirect from="*" to="/" />  
-    </Switch>
+    <>
+      <Header />
+      {/* Switch : 한번에 오직 하나의 Route만 Render하라 */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" component={Search} />
+        {/* 일치하는 Route가 없다면 Redirect 해라 */}
+        <Redirect from="*" to="/" />  
+      </Switch>
+    </>
   </Router>
 )
