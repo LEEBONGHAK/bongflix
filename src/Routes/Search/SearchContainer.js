71dbe5221088bc94a742c5d92fc87a3e35fa5620
octo <dlqhdgkr456@gmail.com>
2021-09-05 14:16:13 +0900

    Search Container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { movieApi, TVApi } from '../../api';
 import SearchPresenter from "./SearchPresenter";
 
 export default class extends React.Component {
@@ -10,6 +11,30 @@ export default class extends React.Component {
     loading: false
   };
 
+  handelSubmit = () => {
+    const { searchTerm } = this.searchTerm;
+    if (searchTerm !== "") {
+      this.searchByTerm(searchTerm);
+    }
+  }
+
+  searchByTerm = async (term) => {
+    this.setState({ loading: true });
+    try {
+      const { data: { results: movieResults } } = await movieApi.search(term);
+      const { data: { results: tvResults } } = await TVApi.search(term);
+      
+      this.setState({
+        movieResults,
+        tvResults
+      })
+    } catch {
+      this.setState({ error: "Can't find results." })
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
     const { movieResult, tvResult, searchTerm, error, loading } = this.state;
     return (
@@ -18,7 +43,9 @@ export default class extends React.Component {
         tvResult={tvResult} 
         searchTerm={searchTerm} 
         error={error} 
-        loading={loading} />
+        loading={loading} 
+        handelSubmit={this.handelSubmit}
+      />
     );
   }
 }
